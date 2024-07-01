df47f3e47ab6eca49dce4457fc9fd64d0bcea1f7
unknown <ricardodonado1105@gmail.com>
2024-07-01 17:01:46 -0500

    adding signUp elements onto JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,4 @@
-console.log("hello world!")
+const mainElement = document.getElementsByTagName("main")[0]
+const errorLabelElement = document.getElementById("errorLabel")
+const inputElement = document.getElementsByTagName("input")[0]
+const sendButtonElement = document.getElementsByClassName("button")[0]
