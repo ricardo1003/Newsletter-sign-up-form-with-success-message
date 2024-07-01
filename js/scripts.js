07be55d7cc648a2b5a6c3223253b50ec91714858
unknown <ricardodonado1105@gmail.com>
2024-07-01 17:57:09 -0500

    The submitted email matches in the success message

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,21 @@ const inputElement = document.getElementsByTagName("input")[0]
 const sendButtonElement = document.getElementsByTagName("button")[0]
 
 const sucessSection = document.getElementsByClassName("successMesage")[0]
+const submittedEmail = document.getElementById("submittedEmail")
 const dismissButton = document.getElementsByTagName("button")[1]
 
 
 sendButtonElement.addEventListener("click",()=>{
     const emailValidation = inputElement.value[(inputElement.value.indexOf("@")+1)] === "."
+    const emailValue = inputElement.value
+
     if (inputElement.value.trim() === "" || inputElement.value.indexOf("@") === -1 || inputElement.value.indexOf(".com") === -1 || emailValidation){
         errorLabelElement.classList.remove("noDisplay")
         inputElement.classList.add("error-state")
     }else{
         errorLabelElement.classList.add("noDisplay")
         inputElement.classList.remove("error-state")
+        submittedEmail.innerHTML = emailValue
         setTimeout(() => {
             mainElement.classList.add("submitted")
             signUpSection.classList.add("noDisplay")
